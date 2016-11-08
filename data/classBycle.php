eff11d1a2914d7bd66c0fcd72da5717faeed94d2
luhaiya <1021380422@qq.com>
2016-11-08 19:28:04 +0800

    添加自行车类

--- data/classBycle.php
+++ data/classBycle.php
@@ -5,28 +5,88 @@
  **功能：自行车类
  **
  **/
+require_once 'commonFunc.php';
+require_once './dataBase/dataBaseOperate.php';
 class Bycle{
 	private $attr;
 	public function __construct($userId){
 		$this->attr = array(
 				'userId'=>$userId,
-				'bikeId'=>0,
 				'imgUrl'=>array(),
 				'tags'=>array(),
 				'brand'=>'',
 				'price'=>'',
 		);
 	}
-	public function uploadImg(){
-		
+	public function saveImgFromPost(){
+		if($_FILE["img"]["name"]){
+			$filename = "../src/imgs/User/" . createStringByTime() . 'jpg';
+			move_uploaded_file($_FILES["img"]["tmp_name"],$filename);
+			$this->attr['imgUrl'][] = $filename;
+			return true;
+		}else{
+			return errorInfo(40001);
+		}
 	}
-	public function getInfoByBikeId(){
-		return json_encode($this->attr);
+	public function addTags($string){
+		$this->attr['tags'][] = $string;
+		return true;
+	}
+	public function addBrand($string){
+		$this->attr['brand'] = $string;
+		return true;
+	}
+	public function addTags($string){
+		$this->attr['price'] = $string;
+		return true;
+	}
+	public function signBycle(){
+		$db = new dBoperate('BycleInfo');
+		if(empty($this->attr['imgUrl'])){
+			return errorInfo(40002);
+		}else{
+			$this->attr['imgUrl'] = json_encode($this->attr['imgUrl']);
+			$this->attr['tags'] = json_encode($this->attr['tags']);
+			$bycleId = $db->insert($this->attr);
+			if($id){
+				return $bycleId;
+			}else{
+				return errorInfo(40003);
+			}
+		}
+	}
+	public function getInfoByBikeId($bycleId){
+		$db = new dBoperate();
+		$sql = 'select * from BycleInfo where id='.$bycleId;
+		$res = $db->query($sql);
+		if(!empty($res)){
+			return json_encode($res);
+		}else{
+			return errorInfo(40004);
+		}	
+	}
+	public function getInfoByUserId($uId){
+		$db = new dBoperate();
+		$sql = 'select * from BycleInfo where userid='.$uId;
+		$res = $db->query($sql);
+		if(!empty($res)){
+			return json_encode($res);
+		}else{
+			return errorInfo(40004);
+		}
+	}
+	public function updateBycleInfo($bycleId){
+	//TODO：用户更新自己自行车的信息
 	}
 	public function getBikeList(){
-	
-	}
-	public function signInBike(){
-		
+	//TODO:搜索出符合用户需求的自行车
+		$db = new dBoperate();
+		$sql = 'select * from BycleInfo';
+		$res = $db->query($sql);
+		if(!empty($res)){
+			return json_encode($res);
+		}else{
+			return errorInfo(40004);
+		}
 	}
 }
--- data/commonFunc.php
+++ data/commonFunc.php
@@ -26,3 +26,47 @@ function httpCurl($url,$type='get',$res='json',$data=''){
 		return $resData;
 	}
 }
+/**
+ **根据时间戳生成字符串，作为文件名
+ **/
+function createStringByTime(){
+	return date('Y').date('m').date('d').date('H').date('i').date('s').date('u');
+}
+/**
+ **@随机生成字符串，作为文件名
+ **@param length指字符串长度
+ **/
+function getRandChar($length){
+	$str = null;
+	$strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+	$max = strlen($strPol)-1;
+	for($i=0;$i<$length;$i++){
+		$str.=$strPol[rand(0,$max)];//rand($min,$max)生成介于min和max两个数之间的一个随机整数
+	}
+	return $str;
+}
+/**
+ **@报错函数
+ **@param errid指错误代码
+ **/
+function errorInfo($errid){
+	$error = array('error'=>1);
+	switch($errid){
+		case 40001:
+			$error['desc']='上传自行车图片失败';
+			break;
+		case 40002:
+			$error['desc']="自行车没有图片";
+			break;
+		case 40003:
+			$error['desc']="自行车注册失败";
+			break;
+		case 40004:
+			$error['desc']="获取自行车信息失败";
+			break;
+		default:
+			$error['desc']='未知错误';
+			break;
+	}
+	return json_encode($error);
+}
